39f316713869c286d90b09c6ab7b04db944b03a1
722499 <240833@edu.p.lodz.pl>
2022-01-21 08:09:23 +0100

    initial commit

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int policz (int a, int b) {
-    return a+b;
+    return a*b;
 }
 
 int main() {
 
-    printf("w pierwszym branchu 2+3=%d",policz(2,3));
+    printf("w pierwszym branchu 2*3=%d",policz(2,3));
     return 0;
 }
